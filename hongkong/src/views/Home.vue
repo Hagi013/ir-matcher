86a979e9a7e16afcf7fb5f0b7adf28b7a826dcd0
Haaagiii <generativepassion@gmail.com>
2018-09-05 01:13:02 +0900

    init front

---
+++ hongkong/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div.home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to OK")
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
