e5a5607ed48bb98017b687c876ebc9d76b944411
Haaagiii <generativepassion@gmail.com>
2018-09-13 23:35:42 +0900

    ready for front dev

---
+++ hongkong/src/views/investor/Login.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .login
+    .container.d-flex.align-items-center
+      .row.justify-content-md-center
+        .col-1 E-MAIL
+        .col-1: input(v-model='mail' type='email')
+      .row.justify-content-md-center
+        .col-1  Password
+        .col-1: input(v-model='password' type='password')
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class Home extends Vue {
+  private mail: string = '';
+  private password: string = '';
+}
+</script>
+
+<style>
+  @import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+</style>
